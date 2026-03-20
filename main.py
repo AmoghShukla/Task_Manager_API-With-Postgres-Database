69e57600a5f1b7152f2bac0384048c1ceb8851d0
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-21 00:03:01 +0530

    refactor: update task route and main application for improved logging and response models

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from utils.Task_logger import logger
+from utils.Task_logger import get_logger
 from routers.Task_Route import router as task_router
 
 app  = FastAPI(title="Task Manager API")
-
 app.include_router(task_router, prefix="/tasks", tags=["Tasks"])
+logger = get_logger(__name__)
 
 @app.get("/")
 def Home():
--- routers/Task_Route.py
+++ routers/Task_Route.py
@@ -1,24 +1,25 @@
-from schema.Task_Schema import Request, Response
-from fastapi import APIRouter, HTTPException
-from utils.Task_logger import logger
+from schema.Task_Schema import TaskResponse, TaskCreate, TaskUpdate
+from fastapi import APIRouter
+from utils.Task_logger import get_logger
 from service.task_service import Task_Service
 
 router = APIRouter()
+logger = get_logger(__name__)
 
-@router.post("/", response_model=Response)
-def create_task(payload : Request):
+@router.post("/", response_model=TaskResponse)
+def create_task(payload : TaskCreate):
     return Task_Service.Create_Task(payload)
 
-@router.get("/", response_model=Response)    
+@router.get("/", response_model=TaskResponse)    
 def Get_All_Task():
     return Task_Service.Get_All_Task()
 
-@router.get("/", response_model=Response)
+@router.get("/", response_model=TaskResponse)
 def Get_Task(task_id : int):
     return Task_Service.Get_Task(task_id)
 
 @router.patch("/")
-def Update_Task(payload):
+def Update_Task(payload : TaskUpdate):
     return Task_Service.Update_Task(payload)
 
 @router.delete("/")
