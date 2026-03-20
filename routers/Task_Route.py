a9428a03a63fcef53df4dec10780e218f3dbdf39
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-20 23:27:56 +0530

    feat: implement task service methods for CRUD operations

--- routers/Task_Route.py
+++ routers/Task_Route.py
@@ -1,6 +1,26 @@
 from schema.Task_Schema import Request, Response
 from fastapi import APIRouter, HTTPException
 from utils.Task_logger import logger
+from service.task_service import Task_Service
 
 router = APIRouter()
 
+@router.post("/", response_model=Response)
+def create_task(payload : Request):
+    return Task_Service.Create_Task(payload)
+
+@router.get("/", response_model=Response)    
+def Get_All_Task():
+    return Task_Service.Get_All_Task()
+
+@router.get("/", response_model=Response)
+def Get_Task(task_id : int):
+    return Task_Service.Get_Task(task_id)
+
+@router.patch("/")
+def Update_Task(payload):
+    return Task_Service.Update_Task(payload)
+
+@router.delete("/")
+def Delete_Task(task_id):
+    return Task_Service.Delete_Task(task_id)
---
+++ service/task_service.py
@@ -0,0 +1,18 @@
+
+
+class Task_Service:
+
+    def Create_Task(Payload):
+        return repository
+    
+    def Get_All_Task():
+        return repository
+    
+    def Get_Task(task_id):
+        return repository
+    
+    def Update_Task(task_id):
+        return repository
+    
+    def Delete_Task(task_id):
+        return repository
