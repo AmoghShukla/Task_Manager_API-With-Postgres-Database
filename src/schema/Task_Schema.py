d6b40be2bf20cb2a51e50a2283236d8110683270
Amogh Shukla <amoghshukla548@gmail.com>
2026-03-26 07:38:33 +0530

    Expadited the Task_Schema

--- src/schema/Task_Schema.py
+++ src/schema/Task_Schema.py
@@ -1,16 +1,35 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 from typing import Optional
 
 class TaskCreate(BaseModel):
-    title : str
-    description : str
+    title: str = Field(min_length=3, max_length=50)
+    description: str = Field(min_length=5, max_length=300)
+
+    @validator("title", "description")
+    def clean(cls, v):
+        v = v.strip()
+        if not v:
+            raise ValueError("Field cannot be empty")
+        return v
+
 
 class TaskResponse(BaseModel):
-    id : int
-    title : str
-    description : Optional[str]
+    id: int
+    title: str
+    description: Optional[str]
+
+    class Config:
+        from_attributes = True
 
 
 class TaskUpdate(BaseModel):
-    title : Optional[str]
-    description : Optional[str]
+    title: Optional[str] = Field(None, min_length=3, max_length=50)
+    description: Optional[str] = Field(None, min_length=5, max_length=300)
+
+    @validator("title", "description")
+    def clean(cls, v):
+        if v is not None:
+            v = v.strip()
+            if not v:
+                raise ValueError("Field cannot be empty")
+        return v
